5d2e2a05e9448d9f2529045bd8dc6293273372f3
saworz <michal.czechowicz7@gmail.com>
2023-11-16 17:51:33 +0100

    Created news feed grid

--- src/Components/MainPage/MainPageView/MainPageView.js
+++ src/Components/MainPage/MainPageView/MainPageView.js
@@ -1,14 +1,15 @@
 import './MainPageView.css';
 import PageTitle from "../PageTitle";
 import Buttons from '../Buttons';
-
+import NewsGrid from '../NewsGrid';
 
 const MainPageView = () => {
 
   const html = 
   <div className="content-div">
     <PageTitle />
-    <Buttons />    
+    <Buttons />
+    <NewsGrid />
   </div>
   
   return (html);
---
+++ src/Components/MainPage/NewsGrid/NewsGrid.js
@@ -0,0 +1,44 @@
+import './NewsGrid.css'
+
+const NewsGrid = () => {
+  
+  return (
+    <div className='page-content'>
+      <div className='news-grid'>
+        <div className='news-container'>
+          1111
+        </div>
+        <div className='news-container'>
+          2222
+        </div>
+        <div className='news-container'>
+          3333
+        </div>
+        <div className='news-container'>
+          4444
+        </div>
+        <div className='news-container'>
+          5555
+        </div>
+        <div className='news-container'>
+          1111
+        </div>
+        <div className='news-container'>
+          2222
+        </div>
+        <div className='news-container'>
+          3333
+        </div>
+        <div className='news-container'>
+          4444
+        </div>
+        <div className='news-container'>
+          5555
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default NewsGrid;
