de6532c4d7c124eda1015a88509486fa6e132272
saworz <michal.czechowicz7@gmail.com>
2023-11-20 20:28:31 +0100

    Added search parameters

--- src/components/SearchNewsPage/SearchNewsPageView/SearchNewsPageView.js
+++ src/components/SearchNewsPage/SearchNewsPageView/SearchNewsPageView.js
@@ -1,7 +1,34 @@
+import './SearchNewsPageView.css';
+import { useState } from 'react';
+
 const SearchNewsPageView = () => {
+  const [searchInputs, setSearchInputs] = useState({
+    query: '',
+    sources: '',
+    domains: '',
+    fromDate: '',
+    toDate: '',
+    language: '',
+    sortBy: '',
+  });
+
+  const handleInputChange = (data) => {
+    const { name, value } = data.target;
+    console.log(name, value)
+  };
+
   return (
-    <div>
-      search news here
+    <div class="content-div">
+      <div class="search-news">
+        <h1>Custom searching query</h1>
+        q: <input type="text" name="query"  onChange={handleInputChange}></input>
+        sources: <input type="text" name="sources" value={searchInputs.sources} onChange={handleInputChange}></input>
+        domains: <input type="text" name="domains" value={searchInputs.domains} onChange={handleInputChange}></input>
+        from: <input type="date" name="fromDate" value={searchInputs.fromDate} onChange={handleInputChange}></input>
+        to: <input type="date" name="toDate" value={searchInputs.toDate} onChange={handleInputChange}></input>
+        language: <input type="text" name="language" value={searchInputs.language} onChange={handleInputChange}></input>
+        sortBy: <input type="text" name="sortBy" value={searchInputs.sortBy} onChange={handleInputChange}></input>
+      </div>  
     </div>
   )
 };
