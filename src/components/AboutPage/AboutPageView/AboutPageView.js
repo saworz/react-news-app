f75c9d8749f301e0a73dfb6f13e007f513168127
saworz <michal.czechowicz7@gmail.com>
2023-11-20 19:42:29 +0100

    Added about page

--- src/components/AboutPage/AboutPageView/AboutPageView.js
+++ src/components/AboutPage/AboutPageView/AboutPageView.js
@@ -1,7 +1,13 @@
+import './AboutPageView.css'
+
 const AboutPageView = () => {
   return (
-    <div>
-      About page
+    <div class="content-div">
+      <div class="about-info">
+        <h1>React News Searching Project</h1>
+        <p>My first react project that uses news api from <a href="https://www.newsapi.org" target="_blank" rel="noreferrer">newsapi.org</a>. On the main page news can be sorted by categories and in the <a href="search-news">Search News</a> tab you can apply more filters, such as country, source, text in content, text in title or date.</p>
+        <p>Check out more of my work on my <a href="https://www.github.com/saworz" target="_blank" rel="noreferrer">Github</a>.</p>
+      </div>
     </div>
   )
 };
