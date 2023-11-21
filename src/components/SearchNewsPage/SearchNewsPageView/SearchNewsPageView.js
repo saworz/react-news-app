095cfea57dbaa83bc8e847a012d5c9fa76902b2e
saworz <michal.czechowicz7@gmail.com>
2023-11-21 12:13:58 +0100

    Added select type options for language and sorting

--- src/components/SearchNewsPage/SearchNewsPageView/SearchNewsPageView.js
+++ src/components/SearchNewsPage/SearchNewsPageView/SearchNewsPageView.js
@@ -1,8 +1,9 @@
 import './SearchNewsPageView.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const SearchNewsPageView = () => {
   const [searchInputs, setSearchInputs] = useState({
+    apikey: '',
     query: '',
     sources: '',
     domains: '',
@@ -18,20 +19,45 @@ const SearchNewsPageView = () => {
       ...prevInputs,
       [name]: value
     }));
-    console.log(searchInputs)
   };
+
+  useEffect(() => {
+    console.log(searchInputs)
+  }, [searchInputs]);
 
   return (
     <div className="content-div">
       <div className="search-news">
         <h1>Custom searching query</h1>
+        API Key: <input type="text" name="apikey" onChange={handleInputChange}></input>
         q: <input type="text" name="query" onChange={handleInputChange}></input>
         sources: <input type="text" name="sources" onChange={handleInputChange}></input>
         domains: <input type="text" name="domains" onChange={handleInputChange}></input>
         from: <input type="date" name="fromDate" onChange={handleInputChange}></input>
         to: <input type="date" name="toDate" onChange={handleInputChange}></input>
-        language: <input type="text" name="language" onChange={handleInputChange}></input>
-        sortBy: <input type="text" name="sortBy" onChange={handleInputChange}></input>
+        language: <select name="language" onChange={handleInputChange}>
+          <option value="">Select language</option>
+          <option value="en">English</option>
+          <option value="ar">Arabic</option>
+          <option value="de">German</option>
+          <option value="es">Spanish</option>
+          <option value="fr">French</option>
+          <option value="he">Hebrew</option>
+          <option value="it">Italian</option>
+          <option value="nl">Dutch</option>
+          <option value="no">Norwegian</option>
+          <option value="pt">Portuguese</option>
+          <option value="ru">Russian</option>
+          <option value="sv">Swedish</option>
+          <option value="zh">Chinese</option>
+        </select>
+        sortBy: <select name="sortBy" onChange={handleInputChange}>
+          <option value="">Sorting type</option>
+          <option value="relevancy">Relevancy</option>
+          <option value="popularity">Popularity</option>
+          <option value="publishedAt">PublishedAt</option>
+
+        </select>
       </div>  
     </div>
   )
