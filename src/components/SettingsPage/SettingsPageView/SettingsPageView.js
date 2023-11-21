35bfdac9bbeaaed82378f316cfbd6ffa26179ae1
saworz <michal.czechowicz7@gmail.com>
2023-11-21 13:41:03 +0100

    Added saving apikey in settings

--- src/components/SettingsPage/SettingsPageView/SettingsPageView.js
+++ src/components/SettingsPage/SettingsPageView/SettingsPageView.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react';
 import './SettingsPageView.css';
 
 const SettingsPageView = () => {
+
+  const [apiKey, setApiKey] = useState(localStorage.getItem('apiKey') || '');
+  
+  const handleApiKeyChange = (data) => {
+    const { value } = data.target;
+    setApiKey(value);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('apiKey', apiKey);
+  }, [apiKey]);
+
   return (
-    <div>
-      aa
+    <div className="content">
+      <div className="settings">
+        <h1>App settings</h1>
+        <h2>Set API Key for NewsAPI.org:</h2>
+        <input type="text" name="apiKey" value={apiKey} onChange={handleApiKeyChange}></input>
+      </div>
     </div>
   );
 };
