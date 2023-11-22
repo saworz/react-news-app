e2ab347737c42b4183567b6731efcf2bb5629406
saworz <michal.czechowicz7@gmail.com>
2023-11-22 17:20:50 +0100

    Displaying NoApiKey in Search News

--- src/components/SearchNewsPage/SearchNewsResults/SearchNewsResults.js
+++ src/components/SearchNewsPage/SearchNewsResults/SearchNewsResults.js
@@ -1,13 +1,24 @@
 import './SearchNewsResults.css';
 import NewsGrid from '../../NewsGrid';
+import validateApiKey from '../../../api/ValidateApiKey';
+import NoApiKey from '../../NoApiKey';
 
 const SearchNewsResults = () => {
-  return (
-    <div className="query-results">
-      <h2>Query results</h2>
-      <NewsGrid newsTopic={'general'}/>
-    </div>
-  );
+  if (validateApiKey()) {
+    return (
+      <div className="query-results">
+        <h2>Query results</h2>
+        <NewsGrid newsTopic={'general'}/>
+      </div>
+    );
+  } else {
+    return (
+      <div className="query-results">
+        <NoApiKey />
+      </div>
+    );
+  };
+
 };
 
 export default SearchNewsResults;
